89f3176da08e857b03d402e1601979b1351023e2
shirley-gu <catherine-ku@outlook.com>
2020-02-23 17:56:27 +0800

    leetcode21

---
+++ 21/solution.go
@@ -0,0 +1,15 @@
+/*21. 合并两个有序链表
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+*/
+package pd
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
